bf3b920b933b03988d926e262bcc4d6e5d58bfc6
Raúl (Station) Pérez <raulytstation@Outlook.com>
2021-05-09 20:37:39 -0400

    Upgraded stationBot stable to 1.0.1c

--- index.js
+++ index.js
@@ -15,8 +15,13 @@ client.on("message", function(message) {
     const args = commandBody.split(' ');
     const command = args.shift().toLowerCase();
 
+    if (command === "botinfo") {
+        const timeTaken = Date.now() - message.createdTimestamp;
+        message.channel.send(`StationBot by stationaryStation\n Version: 1.0.1c\n Current Branch: Stable(github)\n Ping: ${timeTaken}ms\n Hosted with: node.js, discord.js and pm2`)
+    }
+
     if (command === "serverinfo") {
-        message.channel.send(`Server name: ${message.guild.name}\n Total Members: ${message.guild.memberCount}`)
+        message.channel.send(`Server name: ${message.guild.name}\n Total Members: ${message.guild.memberCount}\n`)
     }
     if (command === "test") {
         message.reply('Hello World!');
@@ -60,11 +65,11 @@ client.on("message", function(message) {
                 message.reply("That user isn't in this guild!");
             }
         } else {
-            message.reply("You didn't mention the user to ban!")
+            message.reply("You didn't mention the user to ban!");
         }
     }
     if (command === "help") {
-        message.reply(`Current Commands:\n st!kick(username) | This command kicks the selected user.\n st!serverinfo | This command list the current user count of a server and the server name.\n st!ping | Pings the bot and tells your current ping.\n st!test | Sends Hello World! to the chat.\n st!ban(username) | Bans the username.`)
+        message.reply(`Current Commands:\n st!kick(username) | This command kicks the selected user.\n st!serverinfo | This command list the current user count of a server and the server name.\n st!ping | Pings the bot and tells your current ping.\n st!test | Sends Hello World! to the chat.\n st!ban(username) | Bans the username.\n st!changenick (wip) | Sets your nickname to what you want.`);
     }
     // if (command === "restart") {
     //     if (message.author.id === '567014451337887744') {
@@ -79,6 +84,17 @@ client.on("message", function(message) {
     // }
     if (command === "forcerestart") {
         process.exit();
+    }
+    if (command === "changenick") {
+       let nickedUser = message.mentions.users.first();
+       let nickReason = args.join(" ").slice(22);
+       if (!nickedUser) return message.channel.send("You didn't mention a valid user!")
+       if (!nickReason) return message.channel.send("Not having a name is not a good nickname.");
+       if(!message.member.hasPermission("MANAGE_NICKNAMES")) return message.channel.send("No permissions!");
+       if(nick.hasPermission("MANAGE_NICKNAMES")) return message.channel.send("No need to change the nickname of someone that can change nicknames as well.");
+       nickedUser.setNickname(NickReason);
+       message.channel.send("Changed " + nickedUser + "'s nickname successfully!");
+
     }
     
 });
