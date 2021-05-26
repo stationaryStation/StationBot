c7330260b1bc8e7f9ff9c7cfb54e250fe78d7644
Raúl (Station) Pérez <raulytstation@Outlook.com>
2021-05-26 15:13:57 -0400

    Added embed for st!urban

--- index.js
+++ index.js
@@ -474,7 +474,20 @@ client.on("message", async message => {
             if (!list.length) {
                 return message.channel.send(`No results found for **${args.join(' ')}**.`);
             }
-            message.channel.send(list[0].definition);
+            const [answer] = list;
+            const trim = (str, max) => ((str.length > max) ? `${str.slice(0, max - 3)}...` : str);
+        const embed = new Discord.MessageEmbed()
+	.setColor('#EFFF00')
+	.setTitle(answer.word)
+	.setURL(answer.permalink)
+	.addFields(
+		{ name: 'Definition', value: trim(answer.definition, 1024) },
+		{ name: 'Example', value: trim(answer.example, 1024) },
+		{ name: 'Rating', value: `${answer.thumbs_up} thumbs up. ${answer.thumbs_down} thumbs down.` },
+        );
+
+        message.channel.send(embed);
+
 	}
     
     
