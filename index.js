e4b220be58854bb003f38e5ef106458ff51fad97
StationaryStation <raulytstation@outlook.com>
2021-06-01 12:53:55 -0400

    Added Technical information

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ client.on("message", async message => {
     if (!message.content.startsWith(prefix) || message.author.bot) return;
     const args = message.content.slice(prefix.length).trim().split(/ +/);
     const command = args.shift().toLowerCase();
-    // lists bot info
+    
     if (command === ""){
         const InvalidEmbed = new Discord.MessageEmbed()
                     .setColor('#FF0057')
@@ -60,14 +60,21 @@ client.on("message", async message => {
                     .setThumbnail(`https://raw.githubusercontent.com/stationaryStation/StationBot/Next/Embeds/Bot%20InvalidCommand.png`)
             message.channel.send(InvalidEmbed)
     }
+    // lists bot info
     if (command === "botinfo") {
         // Create a Embed message with bot information
+        const timeTaken = time.now() - message.createdTimestamp();
         const botInfoEmbed = new Discord.MessageEmbed()
 	.setColor('#FF0057')
 	.setTitle(`StationBot ${config.botVer}`)
 	.setURL('https://github.com/stationaryStation/stationBot')
 	.setAuthor('stationaryStation', 'https://user-images.githubusercontent.com/81704775/118518156-d919d380-b705-11eb-9145-bb282e626d3a.png', 'https://github.com/stationaryStation')
 	.setDescription(`StationBot by stationaryStation\nA bot for moderation made with the powerful discord.js engine.`)
+    .addFields(
+        {name: 'Ping', value: `${timeTaken}`},
+        {name: 'Current Version', value: `${config.botVer}`},
+        {name: 'Github Branch', value: 'Pre-Release 1'}
+    )
 	.setThumbnail('https://raw.githubusercontent.com/stationaryStation/StationBot/Next/Embeds/Bot%20Info.png')
     .setFooter('This is pre-release software, this might be unstable at times. Please refer to github.com/stationaryStation/StationBot/issues to report any bug.')
         // Then send the embed to the user's channel
