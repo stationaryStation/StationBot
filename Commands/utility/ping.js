09792df84fa481ed7f2d306b01c677de8fa7b383
StationaryStation <raulytstation@outlook.com>
2021-06-07 12:31:42 -0400

    Added cooldown to ping.js

--- Commands/utility/ping.js
+++ Commands/utility/ping.js
@@ -1,6 +1,7 @@
 module.exports = {
     name: 'ping',
     description: 'Tells the User his ping with an embed',
+    cooldown: 3,
     // eslint-disable-next-line no-unused-vars
     execute(message, args, botVer, prefix){
         const timeTaken = Date.now() - message.createdTimestamp;
