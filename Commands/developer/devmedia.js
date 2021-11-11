4cf31a8a1f7c3d6ed5a726aebf490c0761f38f11
StationaryStation <raulytstation@outlook.com>
2021-11-11 18:23:23 -0400

    Added gitlab to devmedia.js

--- Commands/developer/devmedia.js
+++ Commands/developer/devmedia.js
@@ -6,6 +6,7 @@ module.exports = {
     wip: false,
     // eslint-disable-next-line no-unused-vars
     execute(message, args){
-        message.channel.send(`Dev's twitter:\n https://twitter.com/dumplingfurry/\n Dev's StackOverflow:\n https://stackoverflow.com/users/15887961/stationarystation?tab=profile\n Dev's Github: https://github.com/stationaryStation `);
+        message.channel.send(`Dev's twitter:\n https://twitter.com/dumplingfurry/\n Dev's StackOverflow:\n https://stackoverflow.com/users/15887961/stationarystation?tab=profile\n Dev's Github: https://github.com/stationaryStation\n StationaryStation's Gitlab: https://gitlab.com/stationarystation`);
+	    console.log(`${message.user.tag} has invoked devmedia.js`)
     }
 }
