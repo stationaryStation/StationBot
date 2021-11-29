ef2ef56d3a1bf99cdea9bfa2c1c9ec8d80f81847
Raul (Station) Perez <raulytstation@outlook.com>
2021-11-29 12:40:51 -0400

    Added More Comments

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 // Add variables for libraries
 const Discord = require('discord.js-12')
-const config = require('./config.json')
 const fs = require('fs')
-
+// Import Config
+const config = require('config.json')
+if (!config.Token) {
+  console.log("No Token Detected.\nPlease add the token in config.json")
+  process.exit()
+} else if (!config.prefix) {
+  console.log("No prefix detected.\nPlease add the prefix in config.json")
+}
 // Required variables
 const botVer = config.botVer
 const client = new Discord.Client()
@@ -25,7 +31,7 @@ for (const folder of commandFolders) {
     client.commands.set(command.name, command)
   }
 }
-// Add the event files (if they exist)
+// Add the event files (if they exist) Not Used. Maybe fixing it in the future.
 // for (const file of eventFiles) {
 //   const event = require(`./events/${file}`);
 //   if (event.once) {
@@ -38,6 +44,7 @@ for (const folder of commandFolders) {
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}.\n Ver: ${botVer}\n Prefix: ${prefix}`)
   console.log('Bot ready for operation.')
+  // Check if you are using the Stable version and change status accordingly
   if (config.stable === true) {
     client.user.setActivity(`${prefix}help for command list. | Using Current Branch`, {
       type: 'LISTENING'
@@ -56,6 +63,7 @@ client.on('ready', () => {
     })
   } else {
     // This is just in case of the bot breaking.
+    // Perhaps put the error message on a constant?
     console.error('An unexpected error has ocurred. Please report the issue to https://github.com/stationaryStation/stationBot/issues')
     // And then after trowing the error, close StationBot.
     process.exit()
@@ -70,13 +78,13 @@ client.on('message', async message => {
   const args = message.content.slice(prefix.length).trim().split(/ +/)
   const command = args.shift().toLowerCase()
   /*
-cooldowns are set with:
+  cooldowns are set with:
 	module.exports = {
 	//..
 	cooldown: <cooldown time (in seconds)>,
 	//..
-}
-on the command file.
+  }
+  on the command file (BEFORE THE EXECUTE FUNCTION).
 */
   if (!client.commands.has(command)) return
   const { cooldowns } = client
@@ -104,13 +112,14 @@ on the command file.
 
   try {
     // If the command exists, run it with the following variables (if required)
+    // Some variables are unused for some commands.
     client.commands.get(command).execute(message, args, botVer, prefix, client)
   } catch (error) {
     // else if the command returns an error, notify the user.
-    console.error(error)
+    console.error(`There was an error while running ${command}.\nError Stack:{$error}`)
     message.reply('There was an error trying to execute that command!')
     message.channel.send(`\`\`\` ${error} \`\`\``)
   }
 })
-// After that, login to the bot account.
-client.login(config.BOT_TOKEN)
+// After that, login to the bot account. Make Sure the variable on the config is called Token and not BOT_TOKEN.
+client.login(config.Token)
